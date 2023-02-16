c5cca2e3d3d0418b8c345edf13d1f1a83120e42b
K <kiyoshi@peakdigital.co.jp>
2023-02-16 12:32:10 +0900

    Remove src/client/__tests__

--- src/client/__tests__/helpers/utils.js
+++
@@ -1,8 +0,0 @@
-export function getBroadcastEvents() {
-  return window.localStorage.setItem.mock.calls
-    .filter((call) => call[0] === "nextauth.message")
-    .map(([eventName, value]) => {
-      const { timestamp, ...rest } = JSON.parse(value)
-      return { eventName, value: rest }
-    })
-}
